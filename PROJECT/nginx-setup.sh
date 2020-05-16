dfde861451c0b0c3fc854ba33517cb381f7df577
kavithaAtluri <kavita.atluri@gmail.com>
2020-05-16 18:20:26 +0530

    modified .deps

--- PROJECT/nginx-setup.sh
+++ PROJECT/nginx-setup.sh
@@ -3,6 +3,6 @@
 source `pwd`/.deps
 
 echo -e "${RB} Hello from Nginx ${N}"
-echo -e "\e[1,31m Hello from Nginx \e[0m"
+echo -e "\e[1;33m Hello from Nginx \e[0m"
 echo -e "\e[33m Hello from Nginx"
  
