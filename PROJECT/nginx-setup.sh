7018f857263bcddf69c2fde7e3b43abe7ce5b5a5
kavithaAtluri <kavita.atluri@gmail.com>
2020-05-17 13:52:05 +0530

    Added Head Function in Deps

--- PROJECT/nginx-setup.sh
+++ PROJECT/nginx-setup.sh
@@ -2,7 +2,10 @@
 
 source `pwd`/.deps
 
+<<COMMENT
 echo -e "${RB} Hello from Nginx ${N}"
 echo -e "\e[1;33m Hello from Nginx \e[0m"
 echo -e "\e[33m Hello from Nginx"
- 
+COMMENT
+
+Head "NGINX/ WEB SETUP"
