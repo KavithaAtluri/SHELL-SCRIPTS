6e31f724cae449322ce61b5dc25c6c817bfc62cd
kavithaAtluri <kavita.atluri@gmail.com>
2020-05-18 18:22:10 +0530

    created student-app

---
+++ PROJECT/app-student.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOG=/tmp/app-student.log
+rm -f $LOG
+source `pwd`/.deps
+
+## Variables
+APPUSER=student
+
+Head "STUDENT APP SETUP"
+BPerform "Create Application User"
+id $APPUSER &>>$LOG
+if [ $? -eq 0 ]; then
+    Stat 0 "Application User Creation"
+else
+    useradd $APPUSER &>>$LOG
+    Stat $? "Application User Creation"
+fi
