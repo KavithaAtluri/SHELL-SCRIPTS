cf69033d967b2f0fdfd2c1bc396f4d865d6f9d7c
kavithaAtluri <kavita.atluri@gmail.com>
2020-05-18 14:45:33 +0530

    updated deps and wesetup

--- PROJECT/nginx-setup.sh
+++ PROJECT/nginx-setup.sh
@@ -20,3 +20,8 @@ Stat $? "Nginx Installation"
 BPerform "Clean Old Web Content"
 rm -rf /usr/share/nginx/html/* &>>$LOG
 Stat $? "Clean Old Web Content"
+
+cd /usr/share/nginx/html/
+BPerform "Download Web Content"
+curl -s https://studentapi-cit.s3-us-west-2.amazonaws.com/studentapp-frontend.tar.gz | tar -xz
+Stat $? "Download Nginx Web Content"
