14a2f85b0672496a9c73c882ccb79d029bcc8104
kavithaAtluri <kavita.atluri@gmail.com>
2020-05-19 16:43:16 +0530

    corrcted

--- PROJECT/app-student.sh
+++ PROJECT/app-student.sh
@@ -6,7 +6,7 @@ source `pwd`/.deps
 
 ## Variables
 APPUSER=student
-TOMCAT_VERSION=$(curl -s https://tomcat.apache.org/download-80.cgi | grep 8.5 | grep -v http | sed -e 's|"| |g' | grep -v href | xargs -n1 | grep ^8)
+TOMCAT-VERSION=$(curl -s https://tomcat.apache.org/download-80.cgi | grep 8.5 | grep -v http | sed -e 's|"| |g' | grep -v href | xargs -n1 | grep ^8)
 
 Head "STUDENT APP SETUP"
 BPerform "Create Application User"
@@ -23,6 +23,6 @@ yum install java -y &>>$LOG
 Stat $? "Installing Java"
 
 BPerform "Downloading Tomcat"
-cd /home/$APPUSER
+cd /home/$APPUSER 
 curl -s https://archive.apache.org/dist/tomcat/tomcat-8/v${TOMCAT-VERSION}/bin/apache-tomcat-${TOMCAT-VERSION}.tar.gz 2>>$LOG| tar -xz &>>$LOG 
 Stat $? "Downloading Tomcat"
